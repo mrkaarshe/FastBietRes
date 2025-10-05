fead3a7344099fc16415b7fda1f7857dec7cd4f3
mrkaarshe <odaykaarshe@gmail.com>
2025-10-05 19:42:34 +0300

    fixed errors

--- server/server.js
+++ server/server.js
@@ -1,38 +1,49 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express"
-import cors from 'cors'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/mongoDB.js";
-import router from './routers/authRout.js';
-import userRouter from './routers/userRoutes.js';
+import router from "./routers/authRout.js";
+import userRouter from "./routers/userRoutes.js";
 
+const app = express();
 
-const app = express()
-app.use(express.json())
+const allowedOrigins = ['https://fast-biet-res.vercel.app'];
+
 app.use(cors({
-  origin: 'https://fast-biet-res.vercel.app',
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS not allowed'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
-const allowedOrigns = ['https://fastbietres.onrender.com/']
-app.use(cookieParser())
+
+app.options('*', cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 4000;
 
-connectDB()
+connectDB();
 
+app.get('/', (req, res) => {
+  res.send('hello from server');
+  console.log('hello from server');
+});
 
-app.get('/',(req,res) =>{
-    res.send('hello from server')
-    console.log('hello from server')
-})
+app.use('/api/auth', router);
+app.use('/api/user', userRouter);
 
-app.use('/api/auth',router)
-app.use('/api/user',userRouter);
-
-
-app.listen(port , ()=>{
-    console.log(`app listenin on  ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
