eb69b180ed4197c92243869197f91b48281b6612
mrkaarshe <odaykaarshe@gmail.com>
2025-10-30 14:16:59 +0300

    added new delete contact contoroler and router

--- server/routes/contactRouter.js
+++ server/routes/contactRouter.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { createContact, getContacts } from '../controllers/contactController.js';
+import { createContact, deleteContact, getContacts } from '../controllers/contactController.js';
 const router = express.Router()
 
 
 
 router.get('/contact',getContacts)
 router.post('/contact',createContact)
-
+router.delete('/contact/:id', deleteContact);
 
 export default router
